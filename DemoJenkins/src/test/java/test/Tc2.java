493b1d104fa2a233ff2e1fa82e534948bdac00c1
CHARAN <CHARAN@CHARAN>
2023-05-15 22:33:41 +0530

    First commit Jenkins

---
+++ DemoJenkins/src/test/java/test/Tc2.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc2 {
+	@Test
+	public void m1()
+	{
+		Reporter.log("Tc2 pass");
+	}
+
+}
